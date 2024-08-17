22b9a957aaadd5315c1a6c2e2fada4cf17f00091
Elliptication <amajha@hershey.k12.pa.us>
2024-08-16 23:13:30 -0400

    app changes day 1

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,22 @@
 # Import FastAPI for backend server communication
 import fastapi
-import ly
 from pydantic import BaseModel
+from enum import Enum
+import abjad
 
 # Create FastAPI app
 app = fastapi.FastAPI()
+
+class NoteType(Enum):
+    QUARTER = "quarter"
+    EIGHTH = "eighth"
+    SIXTEENTH = "sixteenth"
+    TRIPLET = "triplet"
+
+class Note(BaseModel):
+    noteType: NoteType
+
+    
 
 
 # Define default root route
